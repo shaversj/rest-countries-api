10db4878aa43eb987de094a3d7cfc569b3319786
Josh Shavers <shaversj@gmail.com>
2025-02-10 08:19:00 -0600

    Add cursor pointer to header button

--- app/routes/Layout.tsx
+++ app/routes/Layout.tsx
@@ -12,10 +12,15 @@ export default function Layout() {
   return (
     <div className={"dark:bg-very-dark-blue-bg min-h-screen bg-[#fafafa] antialiased"}>
       <header
-        className={"shadow-header dark:bg-dark-blue-elements border-0.31 flex h-20 items-center border-white bg-white px-4 lg:px-20 dark:border-0"}
+        className={
+          "shadow-header dark:bg-dark-blue-elements border-0.31 flex h-20 items-center border-white bg-white px-4 lg:px-20 dark:border-0"
+        }
       >
         <h1 className={"text-header"}>Where in the world?</h1>
-        <button onClick={() => toggleDarkMode()} className={"ml-auto flex items-center gap-x-2"}>
+        <button
+          onClick={() => toggleDarkMode()}
+          className={"ml-auto flex cursor-pointer items-center gap-x-2"}
+        >
           <img className={"size-4 lg:size-5"} src={"/moon.svg"} alt={"Dark Mode"} />
           <span className={"text-header-sm"}>{isDarkModeEnabled ? "Light Mode" : "Dark Mode"}</span>
         </button>
